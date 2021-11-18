d67a430fda054262610b9e6fda96904a73dd20bd
Sam <admin@minemint.app>
2021-11-18 00:07:40 -0500

    More Improvements to the TileMap, two layers, want two More

--- src/data/maps/tilemap.js
+++ src/data/maps/tilemap.js
@@ -1,8 +1,8 @@
 import TileSet from '../../systems/gfx/tileset.js';
 
 class TileMap extends TileSet{
-    constructor(name, image, tileSizeX, tileSizeY, tileRowCount, tileColumnCount, grid_conf, gfx) {
-        super(name, image, tileSizeX, tileSizeY, tileColumnCount);
+    constructor(name, image, tileSizeX, tileSizeY, tileRowCount, tileColumnCount, grid_conf, map, gfx) {
+        super(name, image, tileSizeX, tileSizeY, tileRowCount, tileColumnCount);
 
         this.name = name;
         this.image = new Image();
@@ -12,29 +12,58 @@ class TileMap extends TileSet{
         this.tileRowCount = tileRowCount;
         this.tileColumnCount = tileColumnCount;
         this.grid_conf = grid_conf;
+        this.bottom = map[0];
+        this.mid = map[1];
 
         this.canvas = gfx.getCanvas();
         this.ctx = gfx.getContext();
     }
 
     draw() {
-        this.layer_one();
+        this.bottom_layer();
+        this.mid_layer();
 
         if(this.grid_conf == true) {
             this.grid();
         }
+
     }
 
-    layer_one() {
+    bottom_layer() {
         var xX = 0;
         var xY = 0;
 
         var i; 
         var ii; 
-      
-        for(i = 0; i <= this.tileRowCount; i++) {
-            for(ii = 0; ii <= this.tileColumnCount; ii++) {
-                this.ctx.drawImage(this.image, 22*this.tileSizeX, 3*this.tileSizeY, this.tileSizeX, this.tileSizeY, xX, xY, this.tileSizeX, this.tileSizeY);
+
+        var terrain_index = 0;
+
+        for(i = 0; i <= this.tileRowCount[0]; i++) {
+            for(ii = 0; ii <= this.tileColumnCount[0]; ii++) {
+                this.ctx.drawImage(this.image, this.getTile(this.bottom[terrain_index] - 1).x, this.getTile(this.bottom[terrain_index] - 1).y, this.tileSizeX, this.tileSizeY, xX, xY, this.tileSizeX, this.tileSizeY);
+                terrain_index++;
+                xX = xX + 32;
+            }
+            xX = 0;
+            xY = xY + 32; 
+        }
+    }
+
+    mid_layer() {
+        var xX = 0;
+        var xY = 0;
+
+        var i; 
+        var ii; 
+
+        var terrain_index = 0;
+
+        for(i = 0; i <= this.tileRowCount[0]; i++) {
+            for(ii = 0; ii <= this.tileColumnCount[0]; ii++) {
+                if(this.mid[terrain_index] != 0) {
+                    this.ctx.drawImage(this.image, this.getTile(this.mid[terrain_index] - 1).x, this.getTile(this.mid[terrain_index] - 1).y, this.tileSizeX, this.tileSizeY, xX, xY, this.tileSizeX, this.tileSizeY);
+                }
+                terrain_index++;
                 xX = xX + 32;
             }
             xX = 0;
@@ -43,32 +72,36 @@ class TileMap extends TileSet{
     }
 
     grid() {
-        var xX = 32;
-        var xY = 32;
+        var xX = 0;
+        var xY = 0;
 
         var i; 
         var ii; 
 
         this.ctx.strokeStyle = "gray";
         this.ctx.beginPath();       
-        for(i = 0; i <= this.tileRowCount; i++) {
-            for(ii = 0; ii <= this.tileColumnCount; ii++) {
+        for(i = 0; i <= this.tileRowCount[0]; i++) {
+            for(ii = 0; ii <= this.tileColumnCount[0]; ii++) {
                 this.ctx.moveTo(0, xX);
-                this.ctx.lineTo((this.tileColumnCount + 1)*this.tileSizeX, xX);
+                this.ctx.lineTo((this.tileColumnCount[0] + 1)*this.tileSizeX, xX);
                 xX = xX + 32;
             }
             xX = 32; 
         }
 
-        for(i = 0; i <= this.tileRowCount; i++) {
-            for(ii = 0; ii <= this.tileColumnCount; ii++) {
+        for(i = 0; i <= this.tileRowCount[0]; i++) {
+            for(ii = 0; ii <= this.tileColumnCount[0]; ii++) {
                 this.ctx.moveTo(xY, 0);
-                this.ctx.lineTo(xY, (this.tileRowCount + 1)*this.tileSizeY);
+                this.ctx.lineTo(xY, (this.tileRowCount[0] + 1)*this.tileSizeY);
                 xY = xY + 32;
             }
             xY = 32; 
         }
         this.ctx.stroke();
+    }
+
+    gridToggle() {
+        this.grid_conf = !this.grid_conf;
     }
 
 }
--- src/systems/gfx/tileset.js
+++ src/systems/gfx/tileset.js
@@ -1,23 +1,24 @@
 class TileSet {
-    constructor(name, image, tileSizeX, tileSizeY, tileColumnCount) {
+    constructor(name, image, tileSizeX, tileSizeY, tileRowCount, tileColumnCount) {
         this.name = name;
         this.image = image;
         this.tileSizeX = tileSizeX;
         this.tileSizeY = tileSizeY;
+        this.tileRowCount = tileRowCount;
         this.tileColumnCount = tileColumnCount;
     }
 
     getTile(tileIndex) {
         return {
-            x: (tileIndex % this.tileColumnCount) * this.tileSizeX,
-            y: Math.floor(tileIndex / this.tileColumnCount) * this.tileSizeY,
+            x: (tileIndex % this.tileColumnCount[1]) * this.tileSizeX,
+            y: Math.floor(tileIndex / this.tileRowCount[1]) * this.tileSizeY,
             width: this.tileSizeX,
             height: this.tileSizeY
         };
     }
 
     getTileIndex(x, y) {
-        return Math.floor(y / this.tileSizeY) * this.tileColumnCount + Math.floor(x / this.tileSizeX);
+        return Math.floor(y / this.tileSizeY) * this.tileColumnCount[1] + Math.floor(x / this.tileSizeX);
     }
 
     getImage() {

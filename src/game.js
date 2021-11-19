2c1b733e84f8006f40f9d0cfed83e8d2e89861ad
Sam <admin@minemint.app>
2021-11-19 02:30:07 -0500

    More Maps to Test and Camera Added

--- src/game.js
+++ src/game.js
@@ -18,6 +18,9 @@ import MaterialColors from './systems/ui/colors/material.js';
 //testing game objects
 import Player from './data/player/player.js';
 
+//testing camera 
+import Camera from './systems/gfx/camera.js';
+
 let canvas = document.getElementById("game_canvas");
 canvas.width = document.body.clientWidth - 0;
 canvas.height = document.body.clientHeight - 0;
@@ -34,10 +37,12 @@ let yY = canvas.height / 2;
 
 var tempBool = false;
 
-let map = await fetch('../src/data/maps/largemaptest.json').then(response => response.json());
+let map = await fetch('../src/data/maps/massivemap.json').then(response => response.json());
 
 let PlayerOne = new Player("PlayerOne", 100, 100, 10, "../src/data/images/actors.png", xX, yY, 32, 32, 4, 3, 4, 0, gfx, input);
 let Map = new TileMap("map", "../src/data/images/tileset.png", 0, 0, map, gfx);
+
+let Cam = new Camera(PlayerOne, 32, 32, gfx);
 
 let btNewGame = new Button([gfx,input,"New Game",canvas.width / 2,canvas.height / 2 + 20,mColors.blue_600(),mColors.blue_200(),mColors.gray_200(),mColors.blue_800(),() => { console.log("new game click"); tempBool = true; }]);
 let btQuitGame = new Button([gfx,input,"Quit Game",canvas.width - 100, canvas.height / 2,mColors.blue_600(),mColors.blue_200(),mColors.gray_200(),mColors.blue_800(),() => { console.log("quit game click"); tempBool = false; }]);
@@ -64,6 +69,7 @@ class Game {
     }
 
     drawGameLayer() {
+        Cam.update(PlayerOne.getLocation().x, PlayerOne.getLocation().y);
         Map.drawBottom();
         PlayerOne.draw();
         Map.drawTop();
---
+++ src/systems/gfx/camera.js
@@ -0,0 +1,39 @@
+class Camera {
+    constructor(player, width, height, gfx) {
+        this.x = player.getLocation().x;
+        this.y = player.getLocation().y;
+        this.width = width;
+        this.height = height;
+        this.ctx = gfx.getContext();
+
+        this.lastX = this.x;
+        this.lastY = this.y;
+    }
+
+    update(x,y) {
+        this.x = x;
+        this.y = y;
+        if(this.x != this.lastX) {
+            if(this.x > this.lastX) {
+                var diff = this.x - this.lastX;
+                this.ctx.translate(-diff, 0);
+            } else if(this.x < this.lastX) {
+                var diff = this.lastX - this.x;
+                this.ctx.translate(diff, 0);
+            }
+        }
+        if(this.y != this.lastY) {
+            if(this.y > this.lastY) {
+                var diff = this.y - this.lastY;
+                this.ctx.translate(0, -diff);
+            } else if(this.y < this.lastY) {
+                var diff = this.lastY - this.y;
+                this.ctx.translate(0, diff);
+            }
+        }
+        this.lastX = this.x;
+        this.lastY = this.y;
+    }
+}
+
+export default Camera;
